b55e3343b3b131fcf4b1b5389313c3ec5280895c
Rahul Panchal <rahulnpanchal50@gmail.com>
2021-08-08 19:29:29 +0530

    Initial Commit

---
+++ ipl-dashboard-server/src/main/java/com/rahulp/ipldashboardserver/IplDashboardServerApplication.java
@@ -0,0 +1,13 @@
+package com.rahulp.ipldashboardserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplDashboardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplDashboardServerApplication.class, args);
+	}
+
+}
