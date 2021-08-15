2cac1a6c38cf0fc198acc0e90f41cec73c7ab4de
Rahul Panchal <rahulnpanchal50@gmail.com>
2021-08-16 00:58:57 +0530

    Displaying years list on match page

--- ipl-dashboard-client/src/pages/Match.js
+++ ipl-dashboard-client/src/pages/Match.js
@@ -1,26 +1,45 @@
 import { React, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { MatchDetail } from '../components/MatchDetail';
+import { NotFound } from './NotFound';
 
 export const Match = () => {
 
     const [ matches, setMatches ] = useState([]);
+    const [ statusCode, setStatusCode ] = useState([]);
+    const [ years, setYears ] = useState([]);
     
     const { teamName, year } = useParams();
 
     useEffect(() => {
         const fetchMatches = async () => {
-            const response = await fetch(`http://localhost:8080/teams/${teamName}/matches?year=${year}`);
-            const data = await response.json();
-            console.log(data);
-            setMatches(data);
+            const matchesResponse = await fetch(`http://localhost:8080/teams/${teamName}/matches?year=${year}`);
+            const matchesData = await matchesResponse.json();
+            const yearsResponse = await fetch(`http://localhost:8080/teams/${teamName}/years`);
+            const yearsData = await yearsResponse.json();
+            if(matchesData.length === 0){
+                setStatusCode(500);
+            } else {
+                setStatusCode(200);
+            }
+            console.log(matchesData);
+            console.log(yearsData);
+            setMatches(matchesData);
+            setYears(yearsData);
         };
         fetchMatches();
     }, [teamName, year]);
 
+    if(statusCode === 500){
+        return <NotFound />
+    }
+
     return (
         <div className="Match">
-            <h1>Match Page</h1>
+            {statusCode === 200 ? <h1>Match Page</h1> : <p></p>}
+            {years.map(year => {
+                return <h5>{year}</h5>
+            })}
             {matches.map(match => {
                 return <MatchDetail key={match.matchId} match={match} teamName={teamName} />
             })}
