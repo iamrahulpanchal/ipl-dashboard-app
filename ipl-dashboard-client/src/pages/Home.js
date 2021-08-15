25aa83f5fa7eaa632c0a7836a434e5a231003d01
Rahul Panchal <rahulnpanchal50@gmail.com>
2021-08-15 22:41:27 +0530

    Displaying the Teams List on the Home Page

--- ipl-dashboard-client/src/pages/Home.js
+++ ipl-dashboard-client/src/pages/Home.js
@@ -1,5 +1,30 @@
-import { React } from 'react';
+import { React, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Home = () => {
-    return <h1>This is Home Page</h1>
+    
+    const [ allTeams, setAllTeams ] = useState({
+        allTeams: []
+    });
+
+    useEffect(() => {
+        const fetchTeams = async () => {
+            const response = await fetch('http://localhost:8080');
+            const data = await response.json();
+            setAllTeams(data);
+            console.log(data);
+        };
+        fetchTeams();
+    }, []);
+
+    const teamPage = "/teams/";
+
+    return(
+        <div className="Home">
+            <h1>Teams List</h1>
+            {allTeams.map(team => {
+                return <Link to={teamPage + team}><h5>{team}</h5></Link>
+            })}
+        </div>
+    );
 }
