66e7e5987420f05670a53a807e8affb30b15b37b
Rahul Panchal <rahulnpanchal50@gmail.com>
2021-08-17 00:43:17 +0530

    Created Header Comp. & Error Page for Invalid URL

--- ipl-dashboard-client/src/App.js
+++ ipl-dashboard-client/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import { Team } from './pages/Team';
 import { Match } from './pages/Match';
 import { Home } from './pages/Home';
+import { NotFound } from './pages/NotFound';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
               <Route path="/" component={Home} exact />
               <Route path="/teams/:teamName" component={Team} exact />
               <Route path="/teams/:teamName/matches/:year" component={Match} exact />
+              <Route path="*" component={NotFound} />
             </Switch>
           </Router>
         </div>
---
+++ ipl-dashboard-client/src/components/Header.js
@@ -0,0 +1,10 @@
+import { React } from 'react';
+
+export const Header = () => {
+    return (
+        <div>
+            <h3>Home Page (IPL Dashboard)</h3>
+            <hr />
+        </div>
+    );
+};
